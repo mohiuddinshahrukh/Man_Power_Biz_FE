16aa097b3503b3fa8d256fa36bd574fe7143f52d
shahrukh <mohiuddinshahrukh@gmail.com>
2023-07-14 02:55:19 +0500

    fixed api call url

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,5 +1,5 @@
 export const backendURL = () => {
-  if (!window.location.pathname.includes("production")) {
+  if (window.location.pathname.includes("localhost")) {
     return `http://localhost:8080/api/v1`;
   } else {
     return `https://usprojbe-production.up.railway.app/api/v1`;
